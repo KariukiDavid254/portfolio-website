5b4d90fb4a95689f4564f9308b38d1ab4a3f898c
KariukiDavid254 <kariukidavidwangari254@gmail.com>
2023-11-09 18:16:30 +0300

    Footer Styling

--- script.js
+++ script.js
@@ -63,3 +63,28 @@ circle.forEach(elem=>{
 ///Mixing Portfolio Section//////////////////////////////
 
 var mixer = mixitup('.portfolio-gallery');
+
+
+//Active menu//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelector('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+
+//Sticky NavbBar//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const header = document.querySelectorAll("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY > 50)
+})
